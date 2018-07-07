36daf0a4ce80474bd74a919664c53e2154bd7390
Luca DEttorre <ldettorre21@gmail.com>
2018-07-07 22:00:22 +0000

    add static and media routes,installed gunicorn,psycopg2

--- guidetest/settings/prod.py
+++ guidetest/settings/prod.py
@@ -1,5 +1,17 @@
 from .base import *
+import dj_database_url
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG",False)
+
+DATABASES = {
+  'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
